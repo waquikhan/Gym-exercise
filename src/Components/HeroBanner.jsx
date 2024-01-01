17df9e85473beac73b8136b611cfd3a0e0798444
waquikhan <103071047+waquikhan@users.noreply.github.com>
2024-01-01 10:11:46 +0530

    added the first component in the home page

---
+++ src/Components/HeroBanner.jsx
@@ -0,0 +1,28 @@
+import { Box, Typography, Button } from '@mui/material'
+import React from 'react'
+
+import HeroBannerImage from "../assets/images/banner.png"
+
+const HeroBanner = () => {
+  return (
+    <Box sx={{
+      mt: { lg: '212px', xs: '70px' },
+      ml: { sm: '50px'}
+    }} position="relative" p="20px">
+      <Typography color="#FF2625" fontWeight="800" fontSize="30px">
+        Fitness Club
+      </Typography>
+      <Typography fontWeight={700} sx={{fontSize: {lg :'44px', xs: '40px'}}} mb="23px" mt="30px">
+        Sweet, Smile <br/> and Repeat
+      </Typography>
+      <Typography fontSize="22px" lineHeight="35px" mb={3}>
+        Check out the most effective exercises
+      </Typography>
+      <Button variant="contained" color="error" href='exercises' sx={{ backgroundColor:'#ff2625', padding:"10px"}} >Explore Exercises </Button>
+      <Typography fontWeight={600} color="#ff2625" fontSize="200px" sx={{ opacity: 0.1, display: {lg: 'block', xs:'none'}}}>Exercises</Typography>
+      <img src={HeroBannerImage} alt="Banner" className='hero-banner-img'/>
+    </Box>
+  )
+}
+
+export default HeroBanner
